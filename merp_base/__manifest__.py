97b86d3daed34076e4fa197e7fa57334b4db6c20
Veronika <veronika.kotovich@gmail.com>
2018-12-16 19:16:12 +0300

    [IMP] merp_base: changed Merp version

--- merp_base/__manifest__.py
+++ merp_base/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'Ventor Base',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.1.0',
     'author': 'Ventor, Xpansa Group',
     'website': 'https://ventor.tech/',
     'installable': True,
