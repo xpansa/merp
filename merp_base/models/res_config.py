8d8a7b7da5db04972eef7a365a9337b5f1f03c0f
Nedas <nedas.zilinskas@gmail.com>
2019-04-29 16:11:10 +0300

    [IMP] MERPWH-176 - add merp_internal_warehouse to settings

--- merp_base/models/res_config.py
+++ merp_base/models/res_config.py
@@ -12,28 +12,42 @@ class StockConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     module_merp_outgoing_routing = fields.Boolean(
-        'Outgoing Routing')
+        string='Outgoing Routing'
+    )
 
     module_merp_custom_access_rights = fields.Boolean(
-        'Custom Access Rights')
+        string='Custom Access Rights',
+    )
 
     module_merp_picking_wave = fields.Boolean(
-        'Picking Wave')
+        string='Picking Wave',
+    )
 
     module_merp_picking_products_skip = fields.Boolean(
-        'Smart Skip of Products')
+        string='Smart Skip of Products',
+    )
 
     module_merp_instant_move = fields.Boolean(
-        'Instant Move')
+        string='Instant Move',
+    )
 
     module_merp_inventory = fields.Boolean(
-        'Ventor/mERP Inventory')
+        string='Ventor/mERP Inventory',
+    )
 
     module_merp_custom_logotype = fields.Boolean(
-        'Use Custom Logo')
+        string='Use Custom Logo',
+    )
 
-    merp_version = fields.Char(string='Ventor/mERP Version',
-        compute='_compute_merp_version', store=False)
+    module_merp_internal_warehouse = fields.Boolean(
+        string='Ventor Internal Warehouse',
+    )
+
+    merp_version = fields.Char(
+        string='Ventor/mERP Version',
+        compute='_compute_merp_version',
+        store=False,
+    )
 
     @api.depends('company_id')
     def _compute_merp_version(self):
