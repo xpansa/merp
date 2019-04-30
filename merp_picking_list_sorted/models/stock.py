be0528ee79be6a4824f24ca3063aecd595444f57
Nedas <nedas.zilinskas@gmail.com>
2019-04-30 10:06:31 +0300

    [IMP] MERPWH-176 - picking operations report to list operations instead of moves first

--- merp_picking_list_sorted/models/stock.py
+++ merp_picking_list_sorted/models/stock.py
@@ -8,13 +8,13 @@ class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
     @api.model
-    def sort_printer_picking_list(self, pack_operation_ids):
+    def sort_printer_picking_list(self, move_line_ids):
         """ sort list of pack operations by configured field
         """
         strategy = self.env.user.company_id.outgoing_routing_strategy
         strategy_order = self.env.user.company_id.outgoing_routing_order
 
-        return pack_operation_ids.sorted(
+        return move_line_ids.sorted(
             key=lambda r: getattr(r.location_id, strategy, 'None'),
             reverse=strategy_order
         )
