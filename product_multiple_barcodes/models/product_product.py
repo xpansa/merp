e8a853109964682403a69000f854c2a7530b2bb2
Vladislav <32717036+Globyson@users.noreply.github.com>
2020-01-03 12:48:38 +0300

    Onshore 8 migrate module product multiple barcodes (#50)
    * [FIX]ONSHORE-8: Change attrs barcode_ids
    
    * [FIX]ONSHORE-8: Remove .idea directory

--- product_multiple_barcodes/models/product_product.py
+++ product_multiple_barcodes/models/product_product.py
@@ -8,13 +8,19 @@ from odoo.osv import expression
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
-    barcode_ids = fields.One2many('product.barcode.multi', 'product_id', string='Additional Barcodes')
+    barcode_ids = fields.One2many(
+        'product.barcode.multi',
+        'product_id',
+        string='Additional Barcodes'
+    )
 
     @api.model
     def _name_search(self, name, args=None, operator='ilike', limit=100, name_get_uid=None):
         args = args or []
         domain = []
         if name:
-            domain = ['|', '|', ('name', operator, name), ('default_code', operator, name), '|', ('barcode', operator, name), ('barcode_ids', operator, name)]
-        product_id = self._search(expression.AND([domain, args]), limit=limit, access_rights_uid=name_get_uid)
+            domain = ['|', '|', ('name', operator, name), ('default_code', operator, name),
+                      '|', ('barcode', operator, name), ('barcode_ids', operator, name)]
+        product_id = self._search(expression.AND([domain, args]),
+                                  limit=limit, access_rights_uid=name_get_uid)
         return self.browse(product_id).name_get()
