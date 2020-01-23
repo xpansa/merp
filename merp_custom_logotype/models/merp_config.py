ab197b0198b8c65d13ba355d6f70f16669f61696
Roman <50017328+Werded@users.noreply.github.com>
2020-01-23 16:34:33 +0300

    [REF] MERPWH-514: migrating merp_custom_logotype to 13.0 (#61)

--- merp_custom_logotype/models/merp_config.py
+++ merp_custom_logotype/models/merp_config.py
@@ -1,7 +1,8 @@
 ﻿# Copyright 2019 VentorTech OU
 # Part of Ventor modules. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, fields, api, _, exceptions
+from odoo import models, fields, api, _
+from odoo.exceptions import Warning
 import base64
 import struct
 import logging
@@ -37,23 +38,23 @@ class MerpConfigSettings(models.TransientModel):
         conf = self.env['merp.config'].sudo()
         for record in self:
             self._validate_merp_logotype(record)
-            conf.set_param('logo.file', record.merp_logotype_file or '')
-            conf.set_param('logo.name', record.merp_logotype_name or '')
+            conf.set_param('logo.file', record.merp_logotype_file or False)
+            conf.set_param('logo.name', record.merp_logotype_name or False)
 
         return res
 
-    def _validate_merp_logotype(self, record):
+    @staticmethod
+    def _validate_merp_logotype(record):
         if not record.merp_logotype_file:
             return False
 
-        dat = base64.decodestring(record.merp_logotype_file)
+        dat = base64.decodebytes(record.merp_logotype_file)
         png = (dat[:8] == b'\211PNG\r\n\032\n' and (dat[12:16] == b'IHDR'))
         if not png:
-            raise exceptions.Warning(
-                _('Apparently, the logotype is not a .png file.'))
+            raise Warning(_('Apparently, the logotype is not a .png file.'))
+
         width, height = struct.unpack('>LL', dat[16:24])
         if int(width) < LOGOTYPE_W or int(height) < LOGOTYPE_H:
-            raise exceptions.Warning(_('The logotype can\'t be less than %sx%s px.')
-                                     % (LOGOTYPE_W, LOGOTYPE_H))
+            raise Warning(_('The logotype can\'t be less than {}x{} px.'.format(LOGOTYPE_W, LOGOTYPE_H)))
 
         return True
