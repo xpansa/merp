7f47c08198d4f827aa4daad213aadc690402109f
pavelsc-dev <77338338+pavelsc-dev@users.noreply.github.com>
2021-01-29 12:50:58 +0300

    JIFFY-5-odoo-reply-to-sender (#127)
    * [ADD] Adding module mail_reply_to_sender
    
    * [FIX] Combine two records in one .xml file and add README description

---
+++ mail_reply_to_sender/README.rst
@@ -0,0 +1,4 @@
+Reply to Sender
+=================
+
+This module updates reply-to header in outgoing e-mails with generic mail address.
---
+++ mail_reply_to_sender/__manifest__.py
@@ -0,0 +1,23 @@
+# pylint: disable=missing-docstring
+# Copyright 2020 VentorTech OU
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0).
+
+{
+    "name": "Mail Reply to Sender",
+    "summary": "Emails in Odoo are sent to generic mail address",
+    "version": "14.0.1.0.0",
+    "category": "Tools",
+    "website": "https://ventor.tech/",
+    "author": "VentorTech OU",
+    "license": "LGPL-3",
+    "depends": [
+        "sale",
+        "base_automation",
+    ],
+    "data": [
+        "data/server_object_lines.xml",
+        "data/auto_action.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
