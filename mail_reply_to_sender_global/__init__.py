7dd4057bcdd51f6ae407da96991c441a99ccc1a6
pavelsc-dev <77338338+pavelsc-dev@users.noreply.github.com>
2021-02-04 10:49:45 +0300

    Renamed module to mail_reply_to_sender_global (#129)

--- mail_reply_to_sender_global/__init__.py
+++ mail_reply_to_sender_global/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2020 VentorTech OU
+# Copyright 2021 VentorTech OU
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0).
 
 from . import data
--- mail_reply_to_sender_global/__manifest__.py
+++ mail_reply_to_sender_global/__manifest__.py
@@ -1,9 +1,9 @@
 # pylint: disable=missing-docstring
-# Copyright 2020 VentorTech OU
+# Copyright 2021 VentorTech OU
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0).
 
 {
-    "name": "Mail Reply to Sender",
+    "name": "Mail Reply to Sender Global",
     "summary": "Emails in Odoo are sent to generic mail address",
     "version": "14.0.1.0.0",
     "category": "Tools",
@@ -15,7 +15,7 @@
         "base_automation",
     ],
     "data": [
-        "data/auto_action.xml",
+        "data/auto_action_data.xml",
     ],
     "installable": True,
     "application": False,
