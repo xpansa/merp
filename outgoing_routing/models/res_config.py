368a48e053381257a62f75ef76d7a08bb3ff9252
VentorTech <10652233+ventor-dev@users.noreply.github.com>
2021-02-21 12:29:18 +0300

    [FIX] Implemented fix for resetting values

--- outgoing_routing/models/res_config.py
+++ outgoing_routing/models/res_config.py
@@ -8,16 +8,16 @@ class StockConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     outgoing_routing_strategy = fields.Selection(
-        string='Picking Strategy', default='location_id.name',
+        string='Picking Strategy',
         related='company_id.outgoing_routing_strategy',
         readonly=False)
 
     outgoing_routing_order = fields.Selection(
-        string='Picking Order', default='0',
+        string='Picking Order',
         related='company_id.outgoing_routing_order',
         readonly=False)
 
     stock_reservation_strategy = fields.Selection(
-        string='Reservation Strategy', default='base',
+        string='Reservation Strategy',
         related='company_id.stock_reservation_strategy',
         readonly=False)
