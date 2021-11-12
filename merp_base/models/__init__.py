63838413697408b3649a82fe408693fe31022368
Igor Kobziev <igor@ventor.tech>
2021-11-12 13:48:35 +0300

    [REM][MERPWH-913] remove modules: merp_base, merp_custom_access_rights, merp_custom_logotype, merp_default_locations, merp_outgoing_routing

--- merp_base/models/__init__.py
+++
@@ -1,7 +0,0 @@
-﻿# Copyright 2019 VentorTech OU
-# Part of Ventor modules. See LICENSE file for full copyright and licensing details.
-
-from . import merp_config
-from . import res_config
-from . import res_users
-from . import stock_picking
--- merp_outgoing_routing/tests/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2019 VentorTech OU
-# Part of Ventor modules. See LICENSE file for full copyright and licensing details.
-
-from . import test_merp_outgoing_routing
-from . import test_merp_quants_location_routing
-from . import test_stock_reservation_by_name
-from . import test_stock_reservation_by_priority
-from . import test_stock_reservation_by_quantity
